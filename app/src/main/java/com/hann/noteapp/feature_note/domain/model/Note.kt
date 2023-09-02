3ec77f01edb7bd8e5257c1f57fa74f514a2928ec
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2023-09-02 16:26:54 +0700

    feat(sql) : setting up the sql database

---
+++ app/src/main/java/com/hann/noteapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,19 @@
+package com.hann.noteapp.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.hann.noteapp.ui.theme.*
+
+@Entity(tableName = "note")
+data class Note(
+    val title : String,
+    val content : String,
+    val timestamp : Long,
+    val color : Int,
+    @PrimaryKey val id : Int? = null
+){
+    companion object {
+        val noteColors = listOf(Purple80, PurpleGrey80, Pink80, Purple40, PurpleGrey40)
+    }
+}
+
